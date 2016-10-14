4ea2d3c56c1a2de7f5efd36f4084ade36cee92c7
April Peng <helloaprilpeng@gmail.com>
2016-10-14 14:39:49 -0700

    add repo query functionality. output info to user interface and add styling

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,20 +1,23 @@
 var apiKey = require('./../.env').apiKey;
-// 
-// exports.getRepos = function(){
-//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//     console.log(apiKey);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
 
 $(function(){
   $("#user-search").submit(function(event){
     event.preventDefault();
     var searchInput = $("#searchInput").val();
+    var numberRepos;
     $.get("https://api.github.com/users/" + searchInput + "?access_token=" + apiKey).then(function(response){
+        $("#userProfile").append("<h3>User: " + response.login + "</h3>")
+          .then(append("<p>User bio: " + response.bio + "/p>"))
         console.log(JSON.stringify(response));
+        var numberRepos = response.public_repos;
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+
+    $.get("https://api.github.com/users/" + searchInput + "/repos?access_token=" + apiKey).then(function(response){
+        for(var i=0; i < response.length; i++){
+          $("#userRepos").append("<div class='well'><a href=" + response[i].html_url + ">" + response[i].html_url + "</a><br>" + response[i].description + "</div>");
+        }
       }).fail(function(error){
         console.log(error.responseJSON.message);
       });
