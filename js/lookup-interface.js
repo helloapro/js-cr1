3351cd25e40f72352db105b07d5c4c84b443b4c2
April Peng <helloaprilpeng@gmail.com>
2016-10-14 16:05:03 -0700

    add more search functionality, first producing up to 10 possible users for search and allowing user to select from there

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,50 +1,25 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/search/users?q=tom+repos:%3E10?access_token=' + apiKey).then(function(response){
-    console.log(JSON.stringify(response));
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
-
 $(function(){
   $("#user-search").submit(function(event){
     event.preventDefault();
+    $("#userResults").empty();
     $("#userRepos").empty();
     $("#login").empty();
     $("#userProfile").empty();
     var searchInput = $("#searchInput").val();
-    var numberRepos;
-    $.get("https://api.github.com/users/" + searchInput + "?access_token=" + apiKey).then(function(response){
-        var bio;
-        if(response.bio === null){
-          bio = "This user has not set up their bio!";
-        } else {
-          bio = response.bio;
-        }
-        $("#login").append(" " + response.login);
-        $("#userProfile").append("<p>User bio: " + bio + "</p>");
-      }).fail(function(error){
-        alert("Not a user. Try again!");
-        console.log(error.responseJSON.message);
-      });
-
-    $.get("https://api.github.com/users/" + searchInput + "/repos?access_token=" + apiKey).then(function(response){
-      for(var i=0; i < response.length; i++){
-        var description;
-        if(response[i].description === null){
-          description = "The user did not set a description :-(";
-        } else {
-          description = response[i].description;
-        }
-
-        $("#userRepos").append("<div class='well'><a href=" + response[i].html_url + ">" + response[i].html_url + "</a><br>" + description + "</div>");
+    $.get("https://api.github.com/search/users?q=" + searchInput + "+repos:%3E10").then(function(response){
+      console.log(JSON.stringify(response));
+      for(var i=0; i < 10; i++){
+        $("#userResults").append("<div class='well user'><img class='img-responsive initial-imgs' src='" + response.items[i].avatar_url + "'><span class='login'>" + response.items[i].login + "</span></div>");
+        $(".user").click(userClick);
       }
+    }).then(function(){
+      $(".user").click(userClick);
     }).fail(function(error){
+      alert("Something errored, or no results returned for your search. Try again!");
       console.log(error.responseJSON.message);
     });
-
 
   });
 });
---
+++ js/user-interface.js
@@ -0,0 +1,37 @@
+var apiKey = require('./../.env').apiKey;
+
+function userClick(){
+    $("#userResults").empty();
+    $("#userRepos").empty();
+    $("#login").empty();
+    $("#userProfile").empty();
+    var user = $(this).find("span.login").text();
+    $.get("https://api.github.com/users/" + user + "?access_token=" + apiKey).then(function(response){
+        var bio;
+        if(response.bio === null){
+          bio = "This user has not set up their bio!";
+        } else {
+          bio = response.bio;
+        }
+        $("#login").append(" " + response.login);
+        $("#userProfile").append("<p>User bio: " + bio + "</p>");
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+
+    $.get("https://api.github.com/users/" + user + "/repos?access_token=" + apiKey).then(function(response){
+      for(var i=0; i < response.length; i++){
+        var description;
+        if(response[i].description === null){
+          description = "The user did not set a description :-(";
+        } else {
+          description = response[i].description;
+        }
+
+        $("#userRepos").append("<div class='well'><a href=" + response[i].html_url + ">" + response[i].html_url + "</a><br>" + description + "</div>");
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+
+}
