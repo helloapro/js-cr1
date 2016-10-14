1f3573028037b07acddc030fde1227492f7f59b9
April Peng <helloaprilpeng@gmail.com>
2016-10-14 15:10:28 -0700

    organize info to print on index.html to preference. add function to alert for nonexistent username, set requirement for search input, check for null valuesin JSON response fields

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,25 +1,50 @@
 var apiKey = require('./../.env').apiKey;
 
+exports.getRepos = function(){
+  $.get('https://api.github.com/search/users?q=tom+repos:%3E10?access_token=' + apiKey).then(function(response){
+    console.log(JSON.stringify(response));
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
 $(function(){
   $("#user-search").submit(function(event){
     event.preventDefault();
+    $("#userRepos").empty();
+    $("#login").empty();
+    $("#userProfile").empty();
     var searchInput = $("#searchInput").val();
     var numberRepos;
     $.get("https://api.github.com/users/" + searchInput + "?access_token=" + apiKey).then(function(response){
-        $("#userProfile").append("<h3>User: " + response.login + "</h3>")
-          .then(append("<p>User bio: " + response.bio + "/p>"))
-        console.log(JSON.stringify(response));
-        var numberRepos = response.public_repos;
+        var bio;
+        if(response.bio === null){
+          bio = "This user has not set up their bio!";
+        } else {
+          bio = response.bio;
+        }
+        $("#login").append(" " + response.login);
+        $("#userProfile").append("<p>User bio: " + bio + "</p>");
       }).fail(function(error){
+        alert("Not a user. Try again!");
         console.log(error.responseJSON.message);
       });
 
     $.get("https://api.github.com/users/" + searchInput + "/repos?access_token=" + apiKey).then(function(response){
-        for(var i=0; i < response.length; i++){
-          $("#userRepos").append("<div class='well'><a href=" + response[i].html_url + ">" + response[i].html_url + "</a><br>" + response[i].description + "</div>");
+      for(var i=0; i < response.length; i++){
+        var description;
+        if(response[i].description === null){
+          description = "The user did not set a description :-(";
+        } else {
+          description = response[i].description;
         }
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
+
+        $("#userRepos").append("<div class='well'><a href=" + response[i].html_url + ">" + response[i].html_url + "</a><br>" + description + "</div>");
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+
+
   });
 });
