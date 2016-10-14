c0b6fb09f098898ce7b8b647fe0f4037b5fde1a0
April Peng <helloaprilpeng@gmail.com>
2016-10-14 13:51:31 -0700

    update query address to github API to return JSON data correctly

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,12 +1,22 @@
 var apiKey = require('./../.env').apiKey;
-// var github = require('github-api');
+// 
+// exports.getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//     console.log(apiKey);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
 
-console.log(apiKey);
-
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+$(function(){
+  $("#user-search").submit(function(event){
+    event.preventDefault();
+    var searchInput = $("#searchInput").val();
+    $.get("https://api.github.com/users/" + searchInput + "?access_token=" + apiKey).then(function(response){
+        console.log(JSON.stringify(response));
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
   });
-};
+});
